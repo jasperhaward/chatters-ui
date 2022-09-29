1ca70eeceb2bde823b6bf3f12a23f876a3ee0d98
Jasper Haward <jasperhaward@virginmedia.com>
2022-09-29 20:11:43 +0100

    refactor: rename loadSession

--- src/context/SessionContext.tsx
+++ src/context/SessionContext.tsx
@@ -3,7 +3,7 @@ import type { Session } from "@types";
 
 export type TSessionContext = [
     session: Session | undefined,
-    loadSession: (session: Session) => void
+    setSession: (session: Session) => void
 ];
 
 export const SessionContext = createContext<TSessionContext>(null as any);
