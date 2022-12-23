d9d9377731f0fe778861fa01e2a8ec5ce3924134
Jasper Haward <jasperhaward@virginmedia.com>
2022-12-23 14:23:37 +0000

    feat: initial filterBy implementation

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,6 +2,27 @@ export function generateId() {
     return crypto.randomUUID();
 }
 
+export function filterBy<T, K extends keyof T>(
+    key: K,
+    query: string,
+    ...args: T[K] extends string ? [] : [(value: T[K]) => string]
+) {
+    const [valueFormatter] = args;
+
+    return (item: T) => {
+        let value = item[key];
+        let formattedValue: string;
+
+        if (valueFormatter) {
+            formattedValue = valueFormatter(value);
+        } else {
+            formattedValue = value;
+        }
+
+        return formattedValue.toUpperCase().includes(query.toUpperCase());
+    };
+}
+
 export function sortAlphabeticallyBy<T>(key: keyof T) {
     return (a: T, b: T) => {
         if (a[key] > b[key]) {
