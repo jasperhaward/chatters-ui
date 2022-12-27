88e53b6e894b7c6a22fb3cae05f03074c3041e68
Jasper Haward <jasperhaward@virginmedia.com>
2022-12-27 15:37:49 +0000

    feat: spinner option on MultiSelect option

--- src/components/common/MultiSelect.tsx
+++ src/components/common/MultiSelect.tsx
@@ -4,6 +4,7 @@ import { Pill, HighlightedText } from "..";
 export interface MultiSelectOption {
     text: string;
     value: string;
+    spinner?: boolean;
 }
 
 export interface MultiSelectProps {
@@ -27,6 +28,7 @@ export function MultiSelect({
                 <Pill
                     key={option.value}
                     disabled={disabled}
+                    spinner={option.spinner}
                     onClick={() => onRemove(option)}
                 >
                     {query ? (
