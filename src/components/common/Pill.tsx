f8ca7ce63dd5c1c7a9844a7e17832a3d60f1cad8
Jasper Haward <jasperhaward@virginmedia.com>
2022-12-27 15:37:29 +0000

    feat: add spinner option to <Pill />

--- src/components/common/Pill.tsx
+++ src/components/common/Pill.tsx
@@ -1,30 +1,31 @@
 import { ComponentChildren } from "preact";
 import styles from "./Pill.module.scss";
-import { IconButton, IconTuple } from "..";
+import { IconButton, Spinner, SpinnerContainer } from "..";
 
 export interface PillProps {
-    icon?: IconTuple;
     disabled?: boolean;
+    spinner?: boolean;
     onClick: () => void;
     children: ComponentChildren;
 }
 
-export function Pill({
-    icon = ["fas", "times"],
-    disabled,
-    onClick,
-    children,
-}: PillProps) {
+export function Pill({ disabled, spinner, onClick, children }: PillProps) {
     return (
         <div className={styles.pill}>
             <span>{children}</span>
-            <IconButton
-                icon={icon}
-                disabled={disabled}
-                size="sm"
-                color="grey-xdark"
-                onClick={onClick}
-            />
+            {spinner ? (
+                <SpinnerContainer>
+                    <Spinner size="xs" color="grey-xdark" />
+                </SpinnerContainer>
+            ) : (
+                <IconButton
+                    icon={["fas", "times"]}
+                    disabled={disabled}
+                    size="sm"
+                    color="grey-xdark"
+                    onClick={onClick}
+                />
+            )}
         </div>
     );
 }
