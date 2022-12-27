839db1dc3bc4e1dc1f7cd2424541304036247ff1
Jasper Haward <jasperhaward@virginmedia.com>
2022-12-27 23:04:23 +0000

    fix: remove spinner functionality <Pill />

--- src/components/common/Pill.tsx
+++ src/components/common/Pill.tsx
@@ -1,31 +1,24 @@
 import { ComponentChildren } from "preact";
 import styles from "./Pill.module.scss";
-import { IconButton, Spinner } from "..";
+import { IconButton } from "..";
 
 export interface PillProps {
     disabled?: boolean;
-    spinner?: boolean;
-    onClick: () => void;
     children: ComponentChildren;
+    onClick: () => void;
 }
 
-export function Pill({ disabled, spinner, onClick, children }: PillProps) {
+export function Pill({ disabled, children, onClick }: PillProps) {
     return (
         <div className={`${styles.pill} ${disabled ? styles.disabled : ""}`}>
             <span>{children}</span>
-            <div className={styles.icon}>
-                {spinner ? (
-                    <Spinner size="xs" color="grey-xdark" />
-                ) : (
-                    <IconButton
-                        icon={["fas", "times"]}
-                        size="sm"
-                        disabled={disabled}
-                        color="grey-xdark"
-                        onClick={onClick}
-                    />
-                )}
-            </div>
+            <IconButton
+                icon={["fas", "times"]}
+                size="sm"
+                disabled={disabled}
+                color="grey-xdark"
+                onClick={onClick}
+            />
         </div>
     );
 }
