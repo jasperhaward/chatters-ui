ed4c02ec16c44b6c5846c2f7faa62134bad3cf88
Jasper Haward <jasperhaward@virginmedia.com>
2022-12-28 00:37:36 +0000

    refactor: <TabbedMenu />

--- src/components/common/TabbedMenu.tsx
+++ src/components/common/TabbedMenu.tsx
@@ -1,5 +1,29 @@
 import styles from "./TabbedMenu.module.scss";
 
+export interface TabbedMenuItemProps<T> {
+    value: T;
+    isSelected: boolean;
+    onClick: (view: T) => void;
+}
+
+export function TabbedMenuItem<T extends string>({
+    value,
+    isSelected,
+    onClick,
+}: TabbedMenuItemProps<T>) {
+    return (
+        <button
+            className={`${styles.menuItem} ${
+                isSelected ? styles.selected : ""
+            }`}
+            onClick={() => onClick(value)}
+        >
+            <div>{value}</div>
+            {isSelected && <div className={styles.indicator} />}
+        </button>
+    );
+}
+
 export interface TabbedMenuProps<T> {
     selected: T;
     options: T[];
@@ -13,20 +37,14 @@ export function TabbedMenu<T extends string>({
 }: TabbedMenuProps<T>) {
     return (
         <div className={styles.menu}>
-            {options.map((option) => {
-                const isSelected = option === selected;
-
-                return (
-                    <button
-                        key={option}
-                        className={isSelected ? styles.selected : undefined}
-                        onClick={() => onSelect(option)}
-                    >
-                        <div>{option}</div>
-                        {isSelected && <div className={styles.indicator} />}
-                    </button>
-                );
-            })}
+            {options.map((option) => (
+                <TabbedMenuItem
+                    key={option}
+                    value={option}
+                    isSelected={option === selected}
+                    onClick={onSelect}
+                />
+            ))}
         </div>
     );
 }
